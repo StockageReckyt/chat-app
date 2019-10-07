e807b604f45dc3a57df86665f285ce2527032fd5
Maxime <maxime.lombardo@gmail.com>
2019-10-07 18:31:37 +0200

    fin chat

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
 import Contact from "./components/Contact";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Contact />
+    <div class="App">
+      <Contact
+        name="React"
+        avatar="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"
+        online={true}
+      />
+      <Contact
+        name="Angular"
+        avatar="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png"
+        online={false}
+      />
+      <Contact
+        name="PHP"
+        avatar="http://pngimg.com/uploads/php/php_PNG7.png"
+        online={false}
+      />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,28 @@
 import React from "react";
 import "./Contact.css";
 
-function Contact(props) {}
+const Contact = props => (
+  <div class="Contact">
+    <img class="avatar" src={props.avatar} alt={props.name} />
+    <div>
+      <h1 class="name">{props.name}</h1>
+      <div>
+        <p class="status">
+          {props.online ? (
+            <div>
+              <p class="status-online"></p>
+              <p class="status-text">Online</p>
+            </div>
+          ) : (
+            <div>
+              <p class="status-offline"></p>
+              <p class="status-text">Offline</p>
+            </div>
+          )}
+        </p>
+      </div>
+    </div>
+  </div>
+);
 
 export default Contact;
